e35e4eb506fcf0793381752ee0205eaa5c8a02da
Fabien Chaillou <fabien.chaillou@ubisoft.com>
2023-12-04 09:37:09 -0500

    Solve advent of code day2

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -3,11 +3,19 @@ import cats.effect.{IO, IOApp}
 
 import scala.io.Source
 
-
 object Day1 extends IOApp.Simple {
   private val lettersAsNumber =
-    Map("one" -> 1, "two" -> 2, "three" -> 3, "four" -> 4, "five" ->5, "six" ->6, "seven" ->7, "eight" -> 8, "nine" -> 9)
-  private val longestNumberAsLetter = lettersAsNumber.keys.map(_.length).max
+    Map(
+      "one" -> 1,
+      "two" -> 2,
+      "three" -> 3,
+      "four" -> 4,
+      "five" -> 5,
+      "six" -> 6,
+      "seven" -> 7,
+      "eight" -> 8,
+      "nine" -> 9
+    )
 
   override def run: IO[Unit] =
     for {
@@ -26,20 +34,24 @@ object Day1 extends IOApp.Simple {
     def splitStringAtNumbers(line: String): List[String | Int] = {
       line.toCharArray.toVector.foldRight(List.empty[String | Int]) {
         (prevChar, list) =>
-          list match
+          list match {
             case Nil if !prevChar.isDigit => s"$prevChar" :: Nil
-            case Nil => prevChar.asDigit :: Nil
-            case ::(head: String, next) if !prevChar.isDigit => s"$prevChar$head" :: next
-            case ::(head: String, next) if prevChar.isDigit => prevChar.asDigit :: head :: next
-            case ::(head: Int, next) if prevChar.isDigit => prevChar.asDigit :: head :: next
+            case Nil                      => prevChar.asDigit :: Nil
+            case ::(head: String, next) if !prevChar.isDigit =>
+              s"$prevChar$head" :: next
+            case ::(head: String, next) if prevChar.isDigit =>
+              prevChar.asDigit :: head :: next
+            case ::(head: Int, next) if prevChar.isDigit =>
+              prevChar.asDigit :: head :: next
             case ::(head: Int, next) => prevChar.toString :: head :: next
+          }
       }
     }
 
     val digitsVector = splitStringAtNumbers(line)
       .flatMap {
         case s: String => extractNumberAsString(s)
-        case i: Int => List(i)
+        case i: Int    => List(i)
       }
 
     val firstDigit = digitsVector.head
@@ -50,15 +62,13 @@ object Day1 extends IOApp.Simple {
 
   private def extractNumberAsString(text: String): List[Int] = {
     def substrings(str: String): Iterator[String] =
-      if(str.isEmpty) Iterator.empty
+      if (str.isEmpty) Iterator.empty
       else {
         str.inits ++ substrings(str.tail)
       }
 
-    substrings(text)
-      .collect {
-        case str if lettersAsNumber.contains(str) => lettersAsNumber(str)
-      }
-      .toList
+    substrings(text).collect {
+      case str if lettersAsNumber.contains(str) => lettersAsNumber(str)
+    }.toList
   }
 }
---
+++ src/main/scala/Day2.scala
@@ -0,0 +1,60 @@
+import cats.effect.kernel.Resource
+import cats.effect.{IO, IOApp}
+
+import scala.io.Source
+
+object Day2 extends IOApp.Simple {
+
+  override def run: IO[Unit] = for {
+    lines <- readInput
+    games = lines.map(Game.parse)
+    input = Possibility(blue = 14, red = 12, green = 13)
+    possibleGames = games.filter(_.isPossible(input))
+    sum = possibleGames.map(_.id).sum
+    _ <- IO.println(s"Part 1 result is $sum")
+    powers = games.map(_.minPossibility.power)
+    summedPowers = powers.sum
+    _ <- IO.println(s"Part 2 result is $summedPowers")
+  } yield ()
+
+  private def readInput: IO[List[String]] =
+    Resource
+      .fromAutoCloseable(IO(Source.fromResource("day2-input.txt")))
+      .use(src => IO(src.getLines().toList))
+}
+
+object Possibility {
+  private val regex = " (\\d{1,2}) (red|green|blue)".r
+
+  def parse(in: String): Possibility = in.split(",").foldLeft(Possibility(0, 0, 0)) { (p, str) =>
+    str match
+      case regex(count, "red") => p.copy(red = count.toInt)
+      case regex(count, "green") => p.copy(green = count.toInt)
+      case regex(count, "blue") => p.copy(blue = count.toInt)
+  }
+}
+
+case class Possibility(blue: Int, red: Int, green: Int) {
+  def isPossible(input: Possibility): Boolean = {
+    blue <= input.blue && red <= input.red && green <= input.green
+  }
+  def power: Int = blue * red * green
+}
+
+case class Game(id: Int, grabs: List[Possibility]) {
+  def isPossible(input: Possibility): Boolean = grabs.forall(_.isPossible(input))
+
+  def minPossibility: Possibility =
+    Possibility(
+      red = grabs.maxBy(_.red).red,
+      green = grabs.maxBy(_.green).green,
+      blue = grabs.maxBy(_.blue).blue,
+    )
+}
+
+object Game {
+  private val regex = "Game (\\d+):(.*+)".r
+
+  def parse(line: String): Game = line match
+    case regex(id, possibilities) => Game(id.toInt, possibilities.split(';').map(Possibility.parse).toList)
+}
